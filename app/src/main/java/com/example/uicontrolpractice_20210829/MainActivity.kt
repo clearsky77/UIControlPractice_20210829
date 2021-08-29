fd175cf66f7a45e9bc08085bed4ddc9ec367b198
clearsky77 <clearsky3264@gmail.com>
2021-08-29 12:24:53 +0900

    입력 문구를 받아서 토스트로 띄워보기

--- app/src/main/java/com/example/uicontrolpractice_20210829/MainActivity.kt
+++ app/src/main/java/com/example/uicontrolpractice_20210829/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.uicontrolpractice_20210829
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//    메시지를 받아와서 토스트로 띄워준다.
+        toastBtn.setOnClickListener {
+            var inputMessage = messageEdt.text.toString()
+            Toast.makeText(this, inputMessage, Toast.LENGTH_SHORT).show()
+
+        }
+
+
     }
+
+
+
+
 }
